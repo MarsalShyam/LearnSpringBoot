7a6f1fcde79e9749483aa07bbf3e8f6e29857929
MarsalShyam <shyam12marsal@gmail.com>
2025-06-08 11:16:28 +0530

    building GEL all Employees REST API

--- postgrelearn/src/main/java/com/postgreLearn/postgrelearn/controller/EmployeeController.java
+++ postgrelearn/src/main/java/com/postgreLearn/postgrelearn/controller/EmployeeController.java
@@ -7,22 +7,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private EmployeeService employeeService;
 
-    //Building Add Emplyee REST API
+    //Building Add Employee REST API
     @PostMapping
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee=employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    //Building get One Employee REST API
     @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
         EmployeeDto employeeDto=employeeService.getEmployeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    //Building GET ALL Employee REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employees=employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
     }
 }
