7841d125c7222301828b971d55125a631d863d48
MarsalShyam <shyam12marsal@gmail.com>
2025-06-08 12:12:55 +0530

    building update REST API

--- postgrelearn/src/main/java/com/postgreLearn/postgrelearn/service/impl/EmployeeServiceImpl.java
+++ postgrelearn/src/main/java/com/postgreLearn/postgrelearn/service/impl/EmployeeServiceImpl.java
@@ -38,4 +38,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         List<Employee> employees=employeeRepository.findAll();
         return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee=employeeRepository.findById(employeeId)
+                .orElseThrow(()->new ResourceNotFoundException("Employee is not exists with given id: "+employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        Employee updatedEmployeeObj=employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
 }
