6b9cc633e5e6fed88821a7a77e3bf66d98578055
MarsalShyam <shyam12marsal@gmail.com>
2025-06-11 15:18:31 +0530

    initial commit starter

---
+++ BankingREST-API/BankingREST-API/src/main/java/com/BankAPI/BankingREST_API/BankingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.BankAPI.BankingREST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingRestApiApplication.class, args);
+	}
+
+}
