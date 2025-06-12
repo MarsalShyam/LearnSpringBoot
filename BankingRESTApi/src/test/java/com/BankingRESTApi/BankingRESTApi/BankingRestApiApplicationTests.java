379458e3b19ca6192eae43b0ab88e58c9fc323fc
MarsalShyam <shyam12marsal@gmail.com>
2025-06-12 15:30:27 +0530

    initial commit

---
+++ BankingRESTApi/src/test/java/com/BankingRESTApi/BankingRESTApi/BankingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BankingRESTApi.BankingRESTApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
