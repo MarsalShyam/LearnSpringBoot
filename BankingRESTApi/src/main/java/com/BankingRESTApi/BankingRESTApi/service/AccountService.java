3e418fb3fed1b33a76ed34fa1f79b60fcc0fc245
MarsalShyam <shyam12marsal@gmail.com>
2025-06-21 19:26:28 +0530

    Get All Accounts REST Api

--- BankingRESTApi/src/main/java/com/BankingRESTApi/BankingRESTApi/service/AccountService.java
+++ BankingRESTApi/src/main/java/com/BankingRESTApi/BankingRESTApi/service/AccountService.java
@@ -3,6 +3,8 @@ package com.BankingRESTApi.BankingRESTApi.service;
 import com.BankingRESTApi.BankingRESTApi.dto.AccountDto;
 import com.BankingRESTApi.BankingRESTApi.entity.Account;
 
+import java.util.List;
+
 public interface AccountService {
 //    Account createAccount(Account account);
 //    AccountDto createAccount(AccountDto accountDto);not working
@@ -10,5 +12,6 @@ public interface AccountService {
     AccountDto getAccountById(Long id);
     AccountDto deposit(Long id,double amount);
     AccountDto withdraw(Long id,double amount);
+    List<AccountDto> getAllAccounts();
 
 }
