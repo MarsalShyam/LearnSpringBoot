4921df7e1b6c7a79d72204493fc421d17644af3e
MarsalShyam <shyam12marsal@gmail.com>
2025-06-26 07:04:35 +0530

    initial commit of ThymeLeaflearning

---
+++ ThymeLeafLearn/src/main/java/com/ThymeLeaf/ThymeLeafLearn/ThymeLeafLearnApplication.java
@@ -0,0 +1,13 @@
+package com.ThymeLeaf.ThymeLeafLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafLearnApplication.class, args);
+	}
+
+}
