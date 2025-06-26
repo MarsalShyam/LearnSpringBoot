26955d9a23fc86dd1ba457032ec59aa13ab74ac1
MarsalShyam <shyam12marsal@gmail.com>
2025-06-26 08:02:33 +0530

    variable-expression

---
+++ ThymeLeafLearn/src/main/java/com/ThymeLeaf/ThymeLeafLearn/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.ThymeLeaf.ThymeLeafLearn.controller;
+
+import com.ThymeLeaf.ThymeLeafLearn.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    //handle method To handle the variable-expression request
+    @GetMapping("/variable-expression")
+    public String variableExpression(Model model){
+        User user=new User("shyam","shyam@gmail.com","Admin","Male");
+        model.addAttribute("User",user);
+        return "variable-expression";
+    }
+}
